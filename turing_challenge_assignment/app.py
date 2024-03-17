7ff499b64c49b7b0f981f7c052c97e0d5e970ef4
Salvador Moreno Gutiérrez <smoreno94@gmail.com>
2024-03-17 22:26:42 +0100

    feat: add minimum gpt-powered chatbot with langchain

--- turing_challenge_assignment/app.py
+++ turing_challenge_assignment/app.py
@@ -1,6 +1,18 @@
+from dotenv import load_dotenv
 import gradio as gr
+import logging
+
+# Logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Dotenv
+dotenv_loaded = load_dotenv()
+logger.info(f"dotenv_loaded: {dotenv_loaded}")
+
 
 from turing_challenge_assignment.modules.chat import dummy_response
 
 
+# Gradio
 gr.ChatInterface(dummy_response).launch()
--- turing_challenge_assignment/modules/chat.py
+++ turing_challenge_assignment/modules/chat.py
@@ -1,5 +1,18 @@
-def dummy_response(message, history):
-    if len(history) % 2 == 0:
-        return f"Yes, I do think that '{message}'"
-    else:
-        return "I don't think so"
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+import logging
+from typing import Union
+
+logger = logging.getLogger()
+
+llm = ChatOpenAI(temperature=1.0, model="gpt-3.5-turbo")
+
+
+def dummy_response(message: str, history: list(Union(HumanMessage, SystemMessage))):
+    chat_history = []
+    for human, system in history:
+        chat_history.append(HumanMessage(content=human))
+        chat_history.append(SystemMessage(content=system))
+    chat_history.append(HumanMessage(content=message))
+    response = llm.invoke(chat_history)
+    return response.content
